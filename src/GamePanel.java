db1db065e2fdb08e471d3325cddf62e9d5665aa7
jackf <jack.freeman1990@gmail.com>
2022-07-18 12:39:16 +0100

    Autoplay added with bugs when turning 180

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -17,7 +17,7 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_HEIGHT=700;
     static final int UNIT_SIZE=35;      //size of each invisible grid.
     static final int GAME_UNITS=(SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE; //total size of array squares available
-    int DELAY=75;      //game speed (HIGHER = SLOWER)
+    int DELAY=200;      //game speed (HIGHER = SLOWER)
     int[] x = new int[GAME_UNITS];    //will hold the x-value of the snakes body
     int[] y = new int[GAME_UNITS];    //will hold the y-value of the snakes body
     int bodyParts=5;       //initial size of the snake
@@ -47,8 +47,6 @@ public class GamePanel extends JPanel implements ActionListener {
     BufferedImage  startScreen = ImageIO.read(new File(path, "startscreen.png"));
     BufferedImage  gameoverBackground = ImageIO.read(new File(path, "gameoverscreen.PNG"));
 
-
-
     //Constructor
     GamePanel() throws IOException {
         random = new Random();
@@ -58,6 +56,7 @@ public class GamePanel extends JPanel implements ActionListener {
         this.addKeyListener(new myKeyAdapter());
     }
 
+    //starts a new timer, generates a new food and turns running to true.
     public void startGame(){
         newFood();
         //game is now running
@@ -68,20 +67,21 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
 
+    //initiate the paint component
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         draw(g);
     }
 
+    //main function that will be called on re paint to display the snake
     public void draw(Graphics g){
         //if running
         if (this.running){
-            /*draw game grid.
+            //draw game grid.
             for (int i=0; i< SCREEN_HEIGHT/UNIT_SIZE; i++){
                 g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
                 g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
             }
-             */
 
             if (!this.memeMode && !this.hardMode) {
                 //draw the food
@@ -157,13 +157,201 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
 
+    //create the x and y co-ord of a new food and redraw it
     public void newFood(){
-        //create the x and y co-ord of a new food and redraw it
-        //some checks for snake body and so on
         this.foodX=random.nextInt((int)SCREEN_WIDTH/UNIT_SIZE)*UNIT_SIZE + UNIT_SIZE/4;
         this.foodY=random.nextInt((int)SCREEN_HEIGHT/UNIT_SIZE)*UNIT_SIZE +UNIT_SIZE/4;
     }
 
+    //convert snake head location to just a grid location
+    private int snakeLocationToInteger(int snakePosition){
+        return snakePosition/UNIT_SIZE;
+    }
+
+    //convert food location to just a grid location intger
+    private int foodLocationToInteger(int foodPosition){
+        return (foodPosition-UNIT_SIZE/4)/UNIT_SIZE;
+    }
+
+    //return distance between snakehead and food in the x direction.
+    private int XColsAway(int xSnakeHead, int xFoodLocation){
+        return Math.abs(xFoodLocation-xSnakeHead);
+    }
+
+    //return distance between snakehead and food in the y direction.
+    private int yRowsAway(int ySnakeHead, int yFoodLocation){
+        return Math.abs(yFoodLocation-ySnakeHead);
+    }
+
+    //return integer of +/-1 in if food location is in front of or behind current head location in the x direction (horizontal)
+    private int relativeX(int xSnakeHead, int xFoodLocation){
+        if (xSnakeHead==xFoodLocation){
+            return 0;
+        }else if (xSnakeHead>xFoodLocation){
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+    //return integer of +/-1 in if food location is in front of or behind current head location in the y direction (vertical)
+    private int relativeY(int ySnakeHead, int yFoodLocation){
+        if (ySnakeHead==yFoodLocation){
+            return 0;
+        }else if (ySnakeHead>yFoodLocation){
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
+    //check is food is in a location one step away and return that direction
+    private char foodOneSquareAway(int xSnakeHead, int ySnakeHead, int xFoodLocation, int yFoodLocation){
+    if (xFoodLocation==xSnakeHead+1 && yFoodLocation==ySnakeHead){
+        return 'R';
+    }
+    if (xFoodLocation==xSnakeHead-1 && yFoodLocation==ySnakeHead){
+        return 'L';
+    }
+    if (yFoodLocation==ySnakeHead+1 && xFoodLocation==xSnakeHead){
+        return 'U';
+    }
+    if (yFoodLocation==ySnakeHead-1 && xFoodLocation==xSnakeHead) {
+        return 'D';
+    }
+    return 'O';
+    }
+
+    //function to check is next step will collide with the body given direction, snake head and snake body.
+    //return true if there is a collision
+    private boolean checkCollideWithSelf(char currentDirection, int[] xSnakeLocations, int[] ySnakeLocations){
+        int xCurrentHead=snakeLocationToInteger(xSnakeLocations[0]);
+        int yCurrentHead=snakeLocationToInteger(ySnakeLocations[0]);
+        //check for the current direction if the next step forward will collide with the snake body
+        switch (currentDirection) {
+            case 'R':
+                for (int i = 1; i < this.bodyParts; i++) {
+                    if (xCurrentHead + 1 == snakeLocationToInteger(x[i])) {
+                        return true;
+                    }
+                }
+                break;
+            case 'L':
+                for (int i = 1; i < this.bodyParts; i++) {
+                    if (xCurrentHead - 1 == snakeLocationToInteger(x[i])) {
+                        return true;
+                    }
+                }
+                break;
+            case 'U':
+                for (int i = 1; i < this.bodyParts; i++) {
+                    if (yCurrentHead - 1 == snakeLocationToInteger(y[i])) {
+                        return true;
+                    }
+                }
+                break;
+            case 'D':
+                for (int i = 1; i < this.bodyParts; i++) {
+                    if (yCurrentHead + 1 == snakeLocationToInteger(y[i])) {
+                        return true;
+                    }
+                }
+                break;
+        }
+        return false;
+    }
+
+    //function that will auto move the snake head towards the food
+    public void autoPlay(){
+        /*System.out.println("----------------------------------");
+        System.out.println("SnakeHead: x " +this.x[0] + " y "+ this.y[0]);
+        System.out.println("SnakeHead: x " + snakeLocationToInteger(this.x[0]) + " y "+ snakeLocationToInteger(this.y[0]));
+        System.out.println("Food location: x " + foodLocationToInteger(this.foodX) + " y "+ foodLocationToInteger(this.foodY));
+        System.out.println("----------------------------------");
+
+        //find the relative location of the food in relation to the snakes current head location.
+
+         */
+        int xRelativeLocation=relativeX(snakeLocationToInteger(this.x[0]), foodLocationToInteger(this.foodX));
+        int yRelativeLocation=relativeY(snakeLocationToInteger(this.y[0]), foodLocationToInteger(this.foodY));
+        System.out.println("Current direction " + this.direction);
+        System.out.println("X relative food location : " + xRelativeLocation);
+        System.out.println("Y relative food location : " + yRelativeLocation);
+        System.out.println("----------------------------------");
+
+
+        //make decisions depending on the location of the food in relation to the snake head.
+        //if the snake is currently in the right row or column only change the perpendicular direction
+        if (xRelativeLocation==0 || yRelativeLocation==0) {
+            if (xRelativeLocation==0) {
+                if (yRelativeLocation==1){
+                    //if food is in the same row and is below the snake head
+                    this.direction='D';
+                } else {
+                    //if food is in the same row and is above the snake head
+                    this.direction='U';
+                }
+            } else {
+                if (xRelativeLocation==1){
+                    //if food is in the same column as the snake head and to the right
+                    this.direction='R';
+                } else {
+                    //if food is in the same column as the snake head and to the left
+                    this.direction='L';
+                }
+            }
+        } else if (xRelativeLocation==1){
+            //if food is to the right of the snake head move right
+            this.direction='R';
+        } else if (xRelativeLocation==-1){
+            //if food is to the left of the snake head move right
+            this.direction='L';
+        }
+
+        //check for collisions and if returns true change direction depending on current location to try to prevent collisions
+        /*
+        if (checkCollideWithSelf(this.direction, this.x, this.y)){
+            switch (this.direction){
+                case 'U':
+                    //going up will collide, and you can't go back on yourself so chose a right/left
+                    if(Math.random() >0.5){
+                        this.direction='R';
+                    } else {
+                        this.direction='L';
+                    }
+                    break;
+                case 'D':
+                    //going down will collide, and you can't go back on yourself so chose a right/left
+                    if(Math.random()>0.5){
+                        this.direction='L';
+                    } else {
+                        this.direction='D';
+                    }
+                    break;
+                case 'R':
+                    //going right will collide, and you can't go back on yourself so chose an up/down
+                    if(Math.random() >0.5){
+                        this.direction='U';
+                    } else {
+                        this.direction='D';
+                    }
+                    break;
+                case 'L':
+                    //going left will collide, and you can't go back on yourself so chose an up/down
+                    if(Math.random() >0.5){
+                        this.direction='D';
+                    } else {
+                        this.direction='U';
+                    }
+                    break;
+            }
+        }
+
+         */
+    }
+
+    //method to move the snake on the screen. Draw the heads new position while drawing each of the other
+    //positions in the next position.
     public void move(){
         for (int i=this.bodyParts; i >0; i--){
             //iterate through entire snake updating its position previous value is now new value
@@ -193,6 +381,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
 
+    //function to increase score and body size if head collides with the food
     public void checkFood(){
         if (x[0]==this.foodX - UNIT_SIZE/4  && y[0]==this.foodY- UNIT_SIZE/4 ){
             //System.out.println("FOOD EATEN");
@@ -202,6 +391,7 @@ public class GamePanel extends JPanel implements ActionListener {
         }
     }
 
+    //checks for snake collinding with edge of the screen as well as its own body
     public void checkCollisions(){
         //check if head collides with the body
         for (int i=bodyParts; i>0; i--){
@@ -238,6 +428,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
 
+    //draw a game over screen with score
     public void gameOver(Graphics g){
         //game over background image
         g.drawImage(this.gameoverBackground, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, null);
@@ -261,11 +452,13 @@ public class GamePanel extends JPanel implements ActionListener {
         }
     }
 
+    //draw a screen on load up
     public void startScreen(Graphics g){
         //display start screen
         g.drawImage(this.startScreen, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, null);
     }
 
+    //reset all variables needed to begin a new game
     public void resetGame(){
         //System.out.println("Reset GAME");
         //reset game variables
@@ -279,7 +472,6 @@ public class GamePanel extends JPanel implements ActionListener {
 
         this.foodEaten=0;          //players score
         this.direction = 'R';   //initial start the snake will move to the right]
-        this.DELAY=100;
         this.running=false;     //game is running true or false
 
         startGame();
@@ -328,11 +520,9 @@ public class GamePanel extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (this.running){
             move();
-            //System.out.println(this.foodX + "," + this.foodY);
-            //System.out.println(x[0] + "," + y[0]);
-            //System.out.println(timer);
             checkFood();
             checkCollisions();
+            autoPlay();
         }
         repaint();
 
